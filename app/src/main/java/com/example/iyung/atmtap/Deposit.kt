ad575fa80002236ddc2a329b9d22b372df4a7851
a Keech <andy.keech@live.ca>
2018-05-26 10:42:38 -0400

    added nfc support to deposit and withdraw

--- app/src/main/java/com/example/iyung/atmtap/Deposit.kt
+++ app/src/main/java/com/example/iyung/atmtap/Deposit.kt
@@ -1,10 +1,33 @@
 package com.example.iyung.atmtap
 
+import android.nfc.NdefMessage
+import android.nfc.NdefRecord
+import android.nfc.NfcAdapter
+import android.nfc.NfcEvent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_deposit.*
+import org.json.JSONException
+import org.json.JSONObject
 
-class Deposit : AppCompatActivity() {
+class Deposit : AppCompatActivity() , NfcAdapter.CreateNdefMessageCallback {
+
+    override fun createNdefMessage(event: NfcEvent?): NdefMessage {
+        val type = "deposit"
+        val dataToSend = JSONObject()
+        try {
+            dataToSend.put("type", type)
+            dataToSend.put("amount", amount.text)
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+
+        val ndefRecord = NdefRecord.createMime("application/json", dataToSend.toString().toByteArray())
+        val ndefMessage = NdefMessage(ndefRecord)
+        println("Sending a message")
+        return ndefMessage
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +38,16 @@ class Deposit : AppCompatActivity() {
             account.text = myIntent.getString("account")
             action.text = myIntent.getString("action")
         }
+
+        val mAdapter = NfcAdapter.getDefaultAdapter(this)
+        if (mAdapter == null) {
+            return
+        }
+
+        if (!mAdapter.isEnabled) {
+            Toast.makeText(this, "Please enable NFC via Settings.", Toast.LENGTH_LONG).show()
+        }
+
+        mAdapter.setNdefPushMessageCallback(this, this)
     }
 }
